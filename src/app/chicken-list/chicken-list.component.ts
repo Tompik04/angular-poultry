8f1b76bb725d6212b7de51fddaf18166ea4ee314
Tompik04 <tompik14@gmail.com>
2020-11-18 22:43:26 -0300

    Agregado de interfaz y componente de la lsita de ventas

--- src/app/chicken-list/chicken-list.component.ts
+++ src/app/chicken-list/chicken-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chicken } from './Chicken';
 
 @Component({
   selector: 'app-chicken-list',
@@ -7,13 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChickenListComponent implements OnInit {
 
-  chicken = {
-    "name":"Pollo Entero",
-    "type":"Fresco",
-    "price":"$160",
-    "stock":"Disponible",
-    "image": "assets/img/rawchicken.png"
+  chickens: Chicken[] = [
+  {
+    name: "Pollo Entero",
+    type: "Fresco",
+    price: 160,
+    stock: 20,
+    image: "assets/img/rawchicken.png",
+    clearance: false,
+  },
+  {
+    name:"Pechugas",
+    type:"Fresco",
+    price: 265,
+    stock: 3,
+    image: "assets/img/breastchicken.png",
+    clearance: false,
+  },
+  {
+    name:"Pata-Muslo",
+    type:"Fresco",
+    price: 170,
+    stock: 2,
+    image: "assets/img/quarterchicken.png",
+    clearance: false,
+  },
+  {
+    name:"Pata-Muslo",
+    type:"Congelado",
+    price: 140,
+    stock: 7,
+    image: "assets/img/quarterchicken.png",
+    clearance: true,
+  },
+  {
+    name:"Alitas",
+    type:"Fresco",
+    price: 90,
+    stock: 0,
+    image: "assets/img/wingschicken.png",
+    clearance: false,
   }
+];
+
 
   constructor() { }
 
