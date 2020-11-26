f3994805bc22e6a40a73eafb4d08b65d9241166e
Tompik04 <tompik14@gmail.com>
2020-11-26 20:22:14 -0300

    Error fixe

--- src/app/chicken-list/chicken-list.component.ts
+++ src/app/chicken-list/chicken-list.component.ts
@@ -1,75 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { ChickenCartService } from '../chicken-cart.service';
-import { Chicken } from './Chicken';
+import { Component, OnInit } from "@angular/core";
+import { ChickenCartService } from "../chicken-cart.service";
+import { Chicken } from "./Chicken";
 
 @Component({
-  selector: 'app-chicken-list',
-  templateUrl: './chicken-list.component.html',
-  styleUrls: ['./chicken-list.component.scss']
+  selector: "app-chicken-list",
+  templateUrl: "./chicken-list.component.html",
+  styleUrls: ["./chicken-list.component.scss"]
 })
-
-
 export class ChickenListComponent implements OnInit {
-
   chickens: Chicken[] = [
-  {
-    name: "Pollo Entero",
-    type: "Fresco",
-    price: 160,
-    stock: 20,
-    image: "assets/img/rawchicken.png",
-    clearance: false,
-    quantity: 0,
-  },
-  {
-    name:"Pechugas",
-    type:"Fresco",
-    price: 265,
-    stock: 3,
-    image: "assets/img/breastchicken.png",
-    clearance: false,
-    quantity: 0,
-  },
-  {
-    name:"Pata-Muslo",
-    type:"Fresco",
-    price: 170,
-    stock: 2,
-    image: "assets/img/quarterchicken.png",
-    clearance: false,
-    quantity: 0,
-  },
-  {
-    name:"Pata-Muslo",
-    type:"Congelado",
-    price: 140,
-    stock: 7,
-    image: "assets/img/quarterchicken.png",
-    clearance: true,
-    quantity: 0,
-  },
-  {
-    name:"Alitas",
-    type:"Fresco",
-    price: 90,
-    stock: 0,
-    image: "assets/img/wingschicken.png",
-    clearance: false,
-    quantity: 0,
-  }
-];
+    {
+      name: "Pollo Entero",
+      type: "Fresco",
+      price: 160,
+      stock: 20,
+      image: "assets/img/rawchicken.png",
+      clearance: false,
+      quantity: 0
+    },
+    {
+      name: "Pechugas",
+      type: "Fresco",
+      price: 265,
+      stock: 3,
+      image: "assets/img/breastchicken.png",
+      clearance: false,
+      quantity: 0
+    },
+    {
+      name: "Pata-Muslo",
+      type: "Fresco",
+      price: 170,
+      stock: 2,
+      image: "assets/img/quarterchicken.png",
+      clearance: false,
+      quantity: 0
+    },
+    {
+      name: "Pata-Muslo Off",
+      type: "Congelado",
+      price: 140,
+      stock: 7,
+      image: "assets/img/quarterchicken.png",
+      clearance: true,
+      quantity: 0
+    },
+    {
+      name: "Alitas",
+      type: "Fresco",
+      price: 90,
+      stock: 0,
+      image: "assets/img/wingschicken.png",
+      clearance: false,
+      quantity: 0
+    }
+  ];
 
-  constructor(private cart: ChickenCartService) { 
-  }
+  constructor(private cart: ChickenCartService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  addToCart(chicken: Chicken){
-
+  addToCart(chicken: Chicken) {
     this.cart.addToCart(chicken);
     chicken.stock -= chicken.quantity;
     chicken.quantity = 0;
   }
-
 }
